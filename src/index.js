0fcb24689d917801e38e82896e08a442ef2e816f
zhdima <zhdima@gmail.com>
2023-01-13 14:57:58 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import debounce from 'lodash.debounce';
+
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputSearch = document.querySelector('input#search-box');
+
+if (!inputSearch) {
+  throw new Error('Error: invalid markup!');
+}
+
+inputSearch.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(evt) {
+  console.log(1);
+}
